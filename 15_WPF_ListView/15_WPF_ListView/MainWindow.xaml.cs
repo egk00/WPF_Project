ebeceaf7c5f87a903b437f3a03a792a1ef84ee39
egk00 <31694207+egk00@users.noreply.github.com>
2019-02-02 22:01:52 +0900

    ListView Simple Sample
    This Project is Simple ListView

--- 15_WPF_ListView/15_WPF_ListView/MainWindow.xaml.cs
+++ 15_WPF_ListView/15_WPF_ListView/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _15_WPF_ListView
 {
@@ -25,26 +13,9 @@ namespace _15_WPF_ListView
             InitializeComponent();
             List<User> items = new List<User>();
             //리스트 뷰 추가
-            items.Add(new User()
-            {
-                Name = "Park",
-                Age = 32,
-                Mail = "Park@gmail.com"
-            });
-
-            items.Add(new User()
-            {
-                Name = "Kim",
-                Age = 42,
-                Mail = "Kim@gmail.com"
-            });
-
-            items.Add(new User()
-            {
-                Name = "Lee",
-                Age = 22,
-                Mail = "Leek@gmail.com"
-            });
+            items.Add(new User(){ Name = "Park", Age = 32, Mail = "Park@gmail.com" });
+            items.Add(new User(){ Name = "Kim", Age = 42, Mail = "Kim@gmail.com" });
+            items.Add(new User(){ Name = "Lee", Age = 22, Mail = "Leek@gmail.com" });
             // ItemSource에 추가
             MyListView.ItemsSource = items;
         }
@@ -55,5 +26,10 @@ namespace _15_WPF_ListView
         public string Name { get; set; }
         public int Age { get; set; }
         public string Mail { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.Age + " years old";
+        }
     }
 }

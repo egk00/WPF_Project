c9bac7f0d87f4543596c5bdd9ad1871c1a49ba13
egk00 <31694207+egk00@users.noreply.github.com>
2019-02-03 00:00:34 +0900

    WPF ListView
    WPF ListView

---
+++ 16_ListView_Grid/16_ListView_Grid/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace _16_ListView_Grid
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+            items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            listView_Grid.ItemsSource = items;
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Mail { get; set; }
+        }
+    }
+}
